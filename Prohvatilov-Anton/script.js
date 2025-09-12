f520a73e5847584198df767fd7468b720db84c8e
hutor9n <prohvatilov.anton@gmail.com>
2025-09-12 11:01:38 +0300

    init

--- Prohvatilov-Anton/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const board = document.querySelector('.game-board');
-
-  for (let i = 0; i < 256; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('game-board-cell');
-    board.appendChild(cell);
-  }
-});
